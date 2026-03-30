7aaf4cbe2c414e6c7ae05e3ed3fedba5325a4398
dingwanren <154835434@qq.com>
2026-03-30 09:49:32 +0800

    fix: 修复线上样式丢失

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import QueryClientProvider from "@/QueryClientProvider";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "RAG Chatbot",
